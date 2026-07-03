c0d1964809af0780d34aed7c851099ca8f524ad0
Ivan Lytnev <fifa2014444@rambler.ru>
2026-07-03 16:12:37 +0300

    add new function sort_by_date and edited code

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(data: list[dict], state="EXECUTED") -> list[dict]:
+def filter_by_state(data: list[dict], state: str="EXECUTED") -> list[dict]:
     result = []
     for item in data:
         if item.get("state") == state:
@@ -6,15 +6,19 @@ def filter_by_state(data: list[dict], state="EXECUTED") -> list[dict]:
     return result
 
 
+def sort_by_date(data: list[dict], descending: bool = True) -> list[dict]:
+    return sorted(data, key=lambda x: x["date"], reverse=descending)
+
+
 if __name__ == "__main__":
     # 1. Создаем тестовый список словарей
     test_data = [
-        {'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591,'state': 'CANCELED','date': '2018-10-14T08:21:33.419441'}
+        {"id": 414288290, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
     # 2. Проверяем работу по умолчанию (должен найти EXECUTED)
@@ -25,3 +29,6 @@ if __name__ == "__main__":
     print("\nПроверка со статусом CANCELED:")
     result_canceled = filter_by_state(test_data, "CANCELED")
     print(result_canceled)
+
+    result_sorted = sort_by_date(test_data)
+    print(f"Сортировка по убыванию:\n{result_sorted}")
