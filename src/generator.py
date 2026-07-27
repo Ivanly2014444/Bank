a30bb229d69109a7dd184881f0987da6af8ffdb9
Ivan Lytnev <fifa2014444@rambler.ru>
2026-07-27 15:34:00 +0300

    finalize logging decorator, docs, tests and styling

--- src/generator.py
+++ src/generator.py
@@ -65,10 +65,11 @@ transactions = [
 def filter_by_currency(data, currency_code):
     """Функция принимает список словарей и фильтрует их по валюте"""
     return (
-        t for t in data
+        t
+        for t in data
         if t.get("operationAmount", {}).get("currency", {}).get("code", "").upper()
         == currency_code.upper()
-)
+    )
 
 
 def transaction_descriptions(data):
